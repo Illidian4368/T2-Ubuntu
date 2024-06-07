b100b7ef82ca9b5b9c026354082c3408f1773ef7
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-06-07 20:06:59 +0530

    Don't use sudo with dkms

--- files/chroot_build.sh
+++ files/chroot_build.sh
@@ -118,9 +118,6 @@ apt-get install -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="
   apfs-dkms \
   dmg2img
 
-apfs_ver=$(ls /usr/src | grep linux-apfs-rw | cut -d "-" -f 4)
-sudo dkms install -m linux-apfs-rw -v ${apfs_ver} -k KVER-t2-${CODENAME}
-
 echo >&2 "===]> Info: Change initramfs format (for grub)... "
 sed -i "s/COMPRESS=lz4/COMPRESS=gzip/g" "/etc/initramfs-tools/initramfs.conf"
 
@@ -133,6 +130,9 @@ printf 'apple-bce' >>/etc/modules-load.d/t2.conf
 #printf '\n### apple-bce start ###\nsnd\nsnd_pcm\napple-bce\n### apple-bce end ###' >>/etc/initramfs-tools/modules
 #printf '\n# display f* key in touchbar\noptions apple-ib-tb fnmode=1\n'  >> /etc/modprobe.d/apple-tb.conf
 #printf '\n# delay loading of the touchbar driver\ninstall apple-ib-tb /bin/sleep 7; /sbin/modprobe --ignore-install apple-ib-tb' >> /etc/modprobe.d/delay-tb.conf
+
+apfs_ver=$(ls /usr/src | grep linux-apfs-rw | cut -d "-" -f 4)
+dkms install -m linux-apfs-rw -v ${apfs_ver} -k KVER-t2-${CODENAME}
 
 echo >&2 "===]> Info: Update initramfs... "
 
