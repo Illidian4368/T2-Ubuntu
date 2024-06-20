dad0f77f99666d36eafd78be46aafac643fefcab
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-06-20 15:43:21 +0530

    Split ISO without zipping

--- build.sh
+++ build.sh
@@ -83,14 +83,14 @@ do
     echo "Error building ${KERNEL_VERSION}-${ALTERNATIVE}"
     exit "${livecd_exitcode}"
   fi
-  ## Zip iso and split it into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
+  ## Split iso into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
   cd "${ROOT_PATH}"
-  zip -s 1500m "${ROOT_PATH}/output/ubuntu-22.04-${KERNEL_VERSION}-${ALTERNATIVE}.zip" "${ROOT_PATH}/ubuntu-22.04-${KERNEL_VERSION}-${ALTERNATIVE}.iso"
+  split -b 1500M -x "${ROOT_PATH}/ubuntu-22.04-${KERNEL_VERSION}-${ALTERNATIVE}.iso" "${ROOT_PATH}/output/ubuntu-22.04-${KERNEL_VERSION}-${ALTERNATIVE}.iso."
 done
 ## Calculate sha256 sums of built ISO
 sha256sum "${ROOT_PATH}"/*.iso >"${ROOT_PATH}/output/sha256-ubuntu-22.04"
 
 find ./ | grep ".iso"
-find ./ | grep ".zip"
+#find ./ | grep ".zip"
 
 exit "${livecd_exitcode}"
